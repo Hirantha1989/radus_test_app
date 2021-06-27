aade5a4f2790967a4223057632bac70c18cac3b4
Hirantha Athurupana <hiranthaathurupana@yahoo.com>
2021-06-27 19:01:08 +0530

    Remove existing configuration details

--- config/config.php
+++ config/config.php
@@ -14,8 +14,8 @@ define("BASEURL", "http://localhost/radus_test_app");
 define("RADUS_RECEIVER_EMAIL", "developer@nulosoft.com.au");
 
 //Mail configurations
-define("MAIL_HOST", 'smtp.mailtrap.io');
-define("MAIL_USERNAME", 'b7134200db02ac');
-define("MAIL_PASSWORD", 'f3280a8a782924');
-define("MAIL_PORT", 2525);
+define("MAIL_HOST", '');
+define("MAIL_USERNAME", '');
+define("MAIL_PASSWORD", '');
+define("MAIL_PORT", '');
 define("MAIL_FROM_ADDRESS", 'radus@mail.com');
